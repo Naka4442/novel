080f48ab60e93bdcbf783e2a81c93e7c376b3cbd
Azot-Nitrogenium <azotova2205@gmail.com>
2024-05-23 13:59:57 +0300

    главное меню

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
   {
     path: '/',
+    name: 'menu',
+    component: () => import('../views/MainMenuView.vue')
+  },
+  {
+    path: '/game',
     name: 'screen',
     component: () => import('../views/ScreenView.vue')
   },
---
+++ src/views/MainMenuView.vue
@@ -0,0 +1,81 @@
+<template>
+    <main>
+        <h1>Главное меню</h1>
+        <section class="buttons">
+            <div class="menu-buttons">
+                <button class="menu-button">Новая игра</button>
+                <button class="menu-button">Продолжить</button>
+                <button class="menu-button">Сохранения</button>
+            </div>
+            <div class="loginout">
+                <router-link :to="{name : 'authorization'}"><button class="login-button">Войти</button></router-link>
+            </div>
+        </section>
+    </main>
+</template>
+
+<script>
+export default{
+
+}
+</script>
+<style scoped>
+main{
+    width: 100%;
+    height: 100vh;
+    background-image: url('../assets/backgrounds/morning.jpg');
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: space-between;
+    font-family: "Everlasting";
+    color: white;
+    
+}
+.buttons{
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+}
+.menu-buttons{
+    display: flex;
+    flex-direction: column;
+    padding: 50px;
+    gap: 10px;
+}
+.menu-button:hover {
+    border: solid;
+	color: white;
+}
+.login-button:hover {
+    border: solid;
+	color: white;
+}
+.menu-button{
+    text-decoration: none;
+    font-weight: normal;
+    font-size: 25px;
+    color: white;
+    background-color: rgba(0, 0, 0, 0.6);
+    width: 22vw;
+    padding: 10px 20px;
+    width: 300px;
+    font-family: "Everlasting";
+    border-radius: 5px;
+}
+.loginout{
+    padding: 20px;
+    display: flex;
+    align-items: flex-end;
+}
+.login-button{
+    width: 100px;
+    height: 50px;
+    border-radius: 30px;
+    font-family: "Everlasting";
+    border: none;
+    font-size: 20px;
+    color: white;
+    background-color: rgba(0, 0, 0, 0.6);
+}
+</style>
